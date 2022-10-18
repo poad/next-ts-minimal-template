53dc19de14269abb302923e0377e4f61c8f3b7d4
Kenji Saito <ken-yo@mbr.nifty.com>
2022-10-18 09:49:55 +0900

    Apply prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,8 @@ const Home = (): JSX.Element => (
       </h1>
 
       <p className={styles.description}>
-        Get started by editing <code className={styles.code}>pages/index.js</code>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/index.js</code>
       </p>
 
       <div className={styles.grid}>
@@ -29,7 +30,10 @@ const Home = (): JSX.Element => (
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
+        <a
+          href="https://github.com/vercel/next.js/tree/master/examples"
+          className={styles.card}
+        >
           <h3>Examples &rarr;</h3>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
@@ -50,7 +54,8 @@ const Home = (): JSX.Element => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
       </a>
     </footer>
   </div>
