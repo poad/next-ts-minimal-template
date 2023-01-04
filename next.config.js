e4fdd2e3a95b5cff61d9d4d3d7008701745ac89f
Kenji Saito <ken-yo@mbr.nifty.com>
2023-01-04 22:09:31 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+const config = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(
+  {
+    reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+    swcMinify: true,
+    cleanDistDir: true,
+    images: {
+      unoptimized: true,
+    },
+  }
+);
+
+module.exports = config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app';
-import * as React from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-const App = ({ Component }: AppProps): JSX.Element => <Component />;
-
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
